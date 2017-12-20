4499e5e9765ef2d0283fe90843d5a49b67a95b16
yehua.zyh <yehua.zyh@alibaba-inc.com>
2017-12-20 16:20:36 +0800

    range test

--- main/sliceTest.go
+++ main/sliceTest.go
@@ -3,9 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	var numbers = make([]int, 3, 5)
-
+	/* 创建切片 */
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
 	printSlice(numbers)
+
+	/* 打印原始切片 */
+	fmt.Println("numbers ==", numbers)
+
+	/* 打印子切片从索引1(包含) 到索引4(不包含)*/
+	fmt.Println("numbers[1:4] ==", numbers[1:4])
+
+	/* 默认下限为 0*/
+	fmt.Println("numbers[:3] ==", numbers[:3])
+
+	/* 默认上限为 len(s)*/
+	fmt.Println("numbers[4:] ==", numbers[4:])
+
+	numbers1 := make([]int, 0, 5)
+	printSlice(numbers1)
+	/* 打印子切片从索引  0(包含) 到索引 2(不包含) */
+	number2 := numbers[:2]
+	printSlice(number2)
+
+	/* 打印子切片从索引 2(包含) 到索引 5(不包含) */
+	number3 := numbers[2:5]
+	printSlice(number3)
+
+	s1 :=make([]int,4,5)
+
+	printSlice(s1);
+	s1[0]=1
+	s1[1]=1
+	s1[2]=1
+
+  	printSlice(s1);
+
+
 }
 
 func printSlice(x []int) {
