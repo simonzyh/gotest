7c7419bf42b68580b5dfcc71fb4fe7fcda0c8f38
yehua.zyh <yehua.zyh@alibaba-inc.com>
2017-12-22 17:45:55 +0800

    map test

---
+++ main/interfaceTest.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Phone interface {
+	call()
+	call1()
+}
+
+type NokiaPhone struct {
+}
+
+func (nokiaPhone NokiaPhone) call() {
+	fmt.Println("I am Nokia, I can call you!")
+}
+func (nokiaPhone NokiaPhone) call1() {
+	fmt.Println("I am Nokia, I can call1 you!")
+}
+
+type IPhone struct {
+}
+
+func (iPhone IPhone) call() {
+	fmt.Println("I am iPhone, I can call you!")
+}
+func (iPhone IPhone) call1() {
+	fmt.Println("I am iPhone, I can call1 you!")
+}
+func main() {
+	var phone Phone
+
+	phone = new(NokiaPhone)
+	phone.call()
+
+	phone = new(IPhone)
+	phone.call()
+
+}
